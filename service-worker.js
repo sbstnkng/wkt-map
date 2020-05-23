9e68e40c9a264def0df92bf5557ecf6b512a9552
sbstnkng <29359422+sbstnkng@users.noreply.github.com>
2020-05-23 02:48:37 +0000

    Deploying to gh-pages from  @ 7fa0688ded829c2db9db832c01fa1ebd1b775780 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wkt-map/precache-manifest.bcccdeac0806768b340dcc3d007c23a1.js"
+  "/wkt-map/precache-manifest.e6366c8e3b7cca3a3367ae900be96330.js"
 );
 
 self.addEventListener('message', (event) => {
