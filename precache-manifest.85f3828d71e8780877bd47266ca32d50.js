c87e94240eab11bdb7fa2532360094ba91952c44
sbstnkng <29359422+sbstnkng@users.noreply.github.com>
2020-06-13 11:20:28 +0000

    Deploying to gh-pages from  @ eeee9b660a6450998fd126256d389da9936db348 🚀

---
+++ precache-manifest.85f3828d71e8780877bd47266ca32d50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa68cef569a6360e61b8c027f2b0b092",
+    "url": "/wkt-map/index.html"
+  },
+  {
+    "revision": "3ed9366f3da81fe8c148",
+    "url": "/wkt-map/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cde6817f15b8e5f6a21d",
+    "url": "/wkt-map/static/css/main.4f53a9e9.chunk.css"
+  },
+  {
+    "revision": "3ed9366f3da81fe8c148",
+    "url": "/wkt-map/static/js/2.2d4a778d.chunk.js"
+  },
+  {
+    "revision": "6791e7fb9584d65ed87d655035c7da95",
+    "url": "/wkt-map/static/js/2.2d4a778d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cde6817f15b8e5f6a21d",
+    "url": "/wkt-map/static/js/main.7b99db41.chunk.js"
+  },
+  {
+    "revision": "0ce46ac25b8008e141d5",
+    "url": "/wkt-map/static/js/runtime-main.86644d06.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wkt-map/precache-manifest.2988e50e02047b5b1aac28cc45e25b78.js"
+  "/wkt-map/precache-manifest.85f3828d71e8780877bd47266ca32d50.js"
 );
 
 self.addEventListener('message', (event) => {
