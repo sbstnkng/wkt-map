fd245477dafd94c9d71a5df02ff9839ffe408aa0
sbstnkng <sebastian.koenig@scout24.com>
2020-07-25 10:40:09 +0200

    refactor header to give map more space

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
 import styles from './header.module.css';
 
 const Header = () => {
+  const { href } = window.location;
   return (
-    <Navbar bg="dark" variant="dark" sticky="top">
-      <Navbar.Brand href="/" className={`${styles.navbarBrand} align-middle`}>
+    <div className={styles.logoWrapper}>
+      <a href={href} className={styles.logo}>
         <i className="fas fa-map-marked fa-2x"></i>
         <span>WKT Map</span>
-      </Navbar.Brand>
-      <Navbar.Collapse className="justify-content-end">
-        <Navbar.Text>
-          <a href="https://github.com/sbstnkng/wkt-map" target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-github fa-2x"></i>
-          </a>
-        </Navbar.Text>
-      </Navbar.Collapse>
-    </Navbar>
+      </a>
+    </div>
   );
 };
 
